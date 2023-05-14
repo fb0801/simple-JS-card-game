6d2c5559786618b91509b1d667c59e471f0c9ae4
fb0801 <fbhatti0801@gmail.com>
2023-05-14 15:06:01 +0100

    lets get started 2

---
+++ deck.js
@@ -0,0 +1,41 @@
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
+
+
+export default class Deck {
+    constructor(cards){
+        this.cards =cards
+    }
+    shuffle(){
+        this.cards.sort((a,b) => Math.random() - .5)
+    }
+}
+
+class Card{
+    constructor(suit, value){
+        this.suit = suit
+        this.value = value
+    }
+}
+
+function freshDeck(){
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+          })
+        })
+}
